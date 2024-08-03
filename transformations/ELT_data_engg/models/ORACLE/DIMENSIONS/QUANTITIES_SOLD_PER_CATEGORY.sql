6db9b5e33cb06597184f80159428cc6e2a22c350
DannyBoy3310 <78155486+DannyBoy3310@users.noreply.github.com>
2024-08-03 10:44:45 +0000

    merged Branch with Dev'

---
+++ transformations/ELT_data_engg/models/ORACLE/DIMENSIONS/QUANTITIES_SOLD_PER_CATEGORY.sql
@@ -0,0 +1,11 @@
+{{ config(alias = get_table_name() ) }}
+
+    select A.category_code, A.category_name, sum(B.Quantity_Sold) as Total_qt_sold, avg(B.Quantity_Sold*unit_selling_price) as Average_Sales_price 
+    from {{ source('main', 'product_summary')}} A 
+    inner join {{ source('main', 'product_sales') }} B
+    on A.item_code = B.item_code 
+    {% if var('filter_name', '') != '' %}
+        where lower(A.category_name) = '{{ "%s" | format(var('filter_name') | lower ) }}'
+    {% endif %}
+    group by A.category_code, A.category_name
+    
